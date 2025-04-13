2459b0037232c8b550c5bc901f9dba32599645cb
CJ Buzz <cj1000buzz@gmail.com>
2025-04-13 12:29:50 +0800

    Sentiment analysis now accounts for overall sentiment score

--- nlp/sentiment_analysis.py
+++ nlp/sentiment_analysis.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -13,6 +14,8 @@ def analyse_sentiments(
     characters_data_dir: str,
     sentiment_scorer: BaseScorer
 ) -> None:
+    """Conducts sentence-by-sentence sentiment analysis and creates a file character-relations.pkl that stores the sentiment scores and interaction counts between every pair of characters."""
+
     main_characters_aliases_file_path = os.path.join(characters_data_dir, 'main_characters_aliases.json')
 
     if not os.path.exists(main_characters_aliases_file_path):
@@ -25,6 +28,8 @@ def analyse_sentiments(
     num_chapters = len(os.listdir(ner_coref_data_dir))
 
     relations_arr = np.zeros((num_chars, num_chars, 2, num_chapters))
+
+    chapter_values_arr = np.zeros((2, num_chapters))
 
     for chapter in tqdm(os.listdir(ner_coref_data_dir)):
         chapter_num = int(chapter.split('-')[1]) - 1
@@ -39,6 +44,8 @@ def analyse_sentiments(
 
             df.loc[idx, 'Sentiment'] = sentiment
 
+        chapter_values_arr[0][chapter_num] = df['Sentiment'].sum()
+        chapter_values_arr[1][chapter_num] = idx
 
         for idx, row in df.iterrows():
             char_list = set(json.loads(row['characters']) + (json.loads(row['speaker']) if type(row['speaker']) is str else []))
@@ -55,8 +62,18 @@ def analyse_sentiments(
 
         df.to_csv(relevant_sentences_file_path, index=False)
 
-    character_relations_file_path = os.path.join(characters_data_dir, 'character-relations.npy')
-    np.save(character_relations_file_path, relations_arr)
+    info = {
+        "relations" : relations_arr,
+        "chapter" : chapter_values_arr,
+        "total" : np.array([
+            chapter_values_arr[0].sum(),
+            chapter_values_arr[1].sum()
+        ])
+    }
+
+    character_relations_file_path = os.path.join(characters_data_dir, 'character-relations.pkl')
+    with open(character_relations_file_path, 'wb') as file:
+        pickle.dump(info, file)
 
     print("Completed Sentiment Analysis!")
 
@@ -64,6 +81,7 @@ def analyse_sentiments(
 def collate_relations(
     characters_data_dir: str,
     deduct_opposing_avg: bool = False,
+    deduct_book_avg: bool = False
 ) -> None:
     main_characters_aliases_file_path = os.path.join(characters_data_dir, 'main_characters_aliases.json')
 
@@ -73,16 +91,22 @@ def collate_relations(
     with open(main_characters_aliases_file_path, 'r') as file: 
         main_char_list = json.load(file)
 
-    arr_fp = os.path.join(characters_data_dir, 'character-relations.npy')
+    pkl_fp = os.path.join(characters_data_dir, 'character-relations.pkl')
 
-    if not os.path.exists(arr_fp):
-        raise FileNotFoundError('Missing character-relations.npy in given directory!')
+    if not os.path.exists(pkl_fp):
+        raise FileNotFoundError('Missing character-relations.pkl in given directory!')
 
-    arr = np.load(arr_fp)
+    with open(pkl_fp, 'rb') as file:
+        info = pickle.load(file)
+
+    arr = info['relations']
 
     store = [[[0, 0] for _ in range(len(main_char_list))]  for _ in range(len(main_char_list))]
 
     char_avgs = np.sum(arr, axis=(1, 3))
+
+    book_avg = info['total'][0]/info['total'][1] if deduct_book_avg else False
+    print(book_avg)
 
     for i in range(len(main_char_list)):
         for j in range(len(main_char_list)):
@@ -93,7 +117,7 @@ def collate_relations(
             opposing_avg = char_avgs[j][0]/char_avgs[j][1] if deduct_opposing_avg else False
             interaction_count = int(np.sum(arr[i][j][1]))
 
-            store[i][j][0] = np.sum(arr[i][j][0]) / interaction_count - opposing_avg if interaction_count else 0
+            store[i][j][0] = np.sum(arr[i][j][0]) / interaction_count - opposing_avg - book_avg if interaction_count else 0
             store[i][j][1] = interaction_count
 
     interactions_fp = os.path.join(characters_data_dir, 'interactions.json')
